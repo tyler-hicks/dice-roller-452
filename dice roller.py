91f4f9369067f456798535ee579f8b8150161f5c
tyler-hicks <63464888+tyler-hicks@users.noreply.github.com>
2020-04-17 13:23:30 -0500

    dice_roller v0.3
    Added functionality where users can roll multiple times in a row by adding x# to the end of their dice roll input, for example: 1d20 + 1d6 + 7 x3, will run those numbers and print out results 3 times.

--- dice roller.py
+++ dice roller.py
@@ -1,9 +1,23 @@
 from random import randrange
 
-neg_counter = 0
-neg_accumulator = []
+# format should be AdB + CdD +... + modifier, where the variables and modifier are integers.
+# Point users to readme file for full format rules.
+# Going off Dungeons and Dragons 5th Edition rules as standard format.
+# This standard means 20 sided die (d20) behave differently in that modifiers apply to each roll, not just to the total.
+# Will document other exceptions as they come up.
+
 
 dice_input = str(input("Enter your dice roll: "))
+neg_counter = 0
+multiplier = None
+
+# below function stays outside loop
+if 'x' in dice_input:
+    multiplier_split = dice_input.split()
+    dirtymultiplier = multiplier_split.pop(-1)
+    multiplier = int(dirtymultiplier.strip('x'))
+    dice_input = " ".join(multiplier_split)
+# below function stays outside loop
 if '-' in dice_input:
     negative_split = dice_input.split()
     for i in negative_split:
@@ -12,37 +26,73 @@ if '-' in dice_input:
             negative_split[neg_counter + 1] = '-' + negative_split[neg_counter + 1]
         neg_counter = neg_counter + 1
     dice_input = " ".join(negative_split)
+# below can stay outside loop?
 initial_split = dice_input.split('+')
-
+# this should be inside loop?
 accumulator = []
+# everything below should definitely be inside loop
 
-for i in initial_split:
-    stripped_strings = i.strip()
-    if 'd' in stripped_strings:
-        secondary_split = i.split('d')
-        number = int(secondary_split[0])
-        sides = int(secondary_split[1])
-        if number < 0:
-            number = -number
-            for x in range(number):
-                result = randrange(1, sides)
-                accumulator = accumulator + [-result]
+if multiplier is not None:
+    for x in range(multiplier):
+        for i in initial_split:
+            stripped_strings = i.strip()
+            if 'd' in stripped_strings:
+                secondary_split = i.split('d')
+                number = int(secondary_split[0])
+                sides = int(secondary_split[1])
+                if number < 0:
+                    number = -number
+                    for x in range(number):
+                        result = randrange(1, sides)
+                        accumulator = accumulator + [-result]
+                else:
+                    for x in range(number):
+                        result = randrange(1, sides)
+                        accumulator = accumulator + [result]
+            else:
+                accumulator = accumulator + [int(i)]
+
+        print(accumulator)
+
+        string_accumulator = []
+        total = 0
+
+        for i in accumulator:
+            string = str(i)
+            string_accumulator = string_accumulator + [string]
+            total = total + i
+        math = " + ".join(string_accumulator)
+        print(math, '=', total)
+        accumulator = []
+        string_accumulator = []
+else:
+    for i in initial_split:
+        stripped_strings = i.strip()
+        if 'd' in stripped_strings:
+            secondary_split = i.split('d')
+            number = int(secondary_split[0])
+            sides = int(secondary_split[1])
+            if number < 0:
+                number = -number
+                for x in range(number):
+                    result = randrange(1, sides)
+                    accumulator = accumulator + [-result]
+            else:
+                for x in range(number):
+                    result = randrange(1, sides)
+                    accumulator = accumulator + [result]
         else:
-            for x in range(number):
-                result = randrange(1, sides)
-                accumulator = accumulator + [result]
-    else:
-        accumulator = accumulator + [int(i)]
+            accumulator = accumulator + [int(i)]
 
-print(accumulator)
+    print(accumulator)
 
-string_accumulator = []
-total = 0
+    string_accumulator = []
+    total = 0
 
-for i in accumulator:
-    string = str(i)
-    string_accumulator = string_accumulator + [string]
-    total = total + i
-math = " + ".join(string_accumulator)
+    for i in accumulator:
+        string = str(i)
+        string_accumulator = string_accumulator + [string]
+        total = total + i
+    math = " + ".join(string_accumulator)
 
-print(math, '=', total)
+    print(math, '=', total)
