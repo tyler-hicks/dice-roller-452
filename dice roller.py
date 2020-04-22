098a859ae9416406fb37e3bdc77c45c6766c0d23
tyler-hicks <63464888+tyler-hicks@users.noreply.github.com>
2020-04-22 10:05:00 -0500

    Update to 0.5
    Added functions to make my own life easier.
    Added critical success and critical failure notifications when rolling d20's.
    Added bad ASCII art at beginning of program.

--- dice roller.py
+++ dice roller.py
@@ -1,21 +1,70 @@
 from random import randrange
 
-history = []
+def rand_num_gen(integer):
+    result = randrange(1, integer+1)
+    if integer == 20 and result == 20:
+        print("critical success: ", end="")
+    if integer == 20 and result == 1:
+        print("critical failure: ", end="")
+    return result
 
+def roller(list):
+    accumulator = []
+    for i in list:
+        stripped_strings = i.strip()
+        if 'd' in stripped_strings:
+            secondary_split = i.split('d')
+            number = int(secondary_split[0])
+            sides = int(secondary_split[1])
+            if number < 0:
+                number = -number
+                for x in range(number):
+                    result = rand_num_gen(sides)
+                    accumulator = accumulator + [-result]
+            else:
+                for x in range(number):
+                    result = rand_num_gen(sides)
+                    accumulator = accumulator + [result]
+        else:
+            accumulator = accumulator + [int(i)]
+    return accumulator
+
+def output(list):
+    string_accumulator = []
+    total = 0
+    for i in list:
+        string = str(i)
+        string_accumulator = string_accumulator + [string]
+        total = total + i
+    if len(string_accumulator) > 1:
+        math = " + ".join(string_accumulator)
+        display_list = [math] + ["="] + [str(total)]
+        display = " ".join(display_list)
+    else:
+        display = str(total)
+    return display
+
+print("""          _________ 
+         | o     o |  
+         |    o    |  
+         | o     o |  
+         '---------'  
+       DICE ROLLER V0.5""")
+
+history = []
 while True:
-    inp = input('roll, history, or close?: ')
+    inp = str(input('roll, history, or close?: '))
+    inp = inp.lower()
     if inp == 'roll':
+        print("Autobots, roll out")
         dice_input = str(input("Enter your dice roll: "))
         neg_counter = 0
         multiplier = None
-
-# below function stays outside loop
         if 'x' in dice_input:
             multiplier_split = dice_input.split()
             dirtymultiplier = multiplier_split.pop(-1)
             multiplier = int(dirtymultiplier.strip('x'))
             dice_input = " ".join(multiplier_split)
-# below function stays outside loop
         if '-' in dice_input:
             negative_split = dice_input.split()
             for i in negative_split:
@@ -24,80 +73,30 @@ while True:
                     negative_split[neg_counter + 1] = '-' + negative_split[neg_counter + 1]
                 neg_counter = neg_counter + 1
             dice_input = " ".join(negative_split)
-# below can stay outside loop?
         initial_split = dice_input.split('+')
-# this should be inside loop?
-        accumulator = []
-# everything below should definitely be inside loop
-
         if multiplier is not None:
             for x in range(multiplier):
-                for i in initial_split:
-                    stripped_strings = i.strip()
-                    if 'd' in stripped_strings:
-                        secondary_split = i.split('d')
-                        number = int(secondary_split[0])
-                        sides = int(secondary_split[1])
-                        if number < 0:
-                            number = -number
-                            for x in range(number):
-                                result = randrange(1, sides)
-                                accumulator = accumulator + [-result]
-                        else:
-                            for x in range(number):
-                                result = randrange(1, sides)
-                                accumulator = accumulator + [result]
-                    else:
-                        accumulator = accumulator + [int(i)]
-                string_accumulator = []
-                total = 0
-                for i in accumulator:
-                    string = str(i)
-                    string_accumulator = string_accumulator + [string]
-                    total = total + i
-                math = " + ".join(string_accumulator)
-                print(math, '=', total)
-                history = history + [(dice_input, total)]
+                accumulator = roller(initial_split)
+                display = output(accumulator)
+                print(display)
+                history = history + [(dice_input, display)]
                 accumulator = []
-                string_accumulator = []
         else:
-            for i in initial_split:
-                stripped_strings = i.strip()
-                if 'd' in stripped_strings:
-                    secondary_split = i.split('d')
-                    number = int(secondary_split[0])
-                    sides = int(secondary_split[1])
-                    if number < 0:
-                        number = -number
-                        for x in range(number):
-                            result = randrange(1, sides)
-                            accumulator = accumulator + [-result]
-                    else:
-                        for x in range(number):
-                            result = randrange(1, sides)
-                            accumulator = accumulator + [result]
-                else:
-                    accumulator = accumulator + [int(i)]
-            string_accumulator = []
-            total = 0
-            for i in accumulator:
-                string = str(i)
-                string_accumulator = string_accumulator + [string]
-                total = total + i
-            math = " + ".join(string_accumulator)
-            print(math, '=', total)
-            history = history + [(dice_input, total)]
-    elif inp == 'history':
+            accumulator = roller(initial_split)
+            display = output(accumulator)
+            print(display)
+            history = history + [(dice_input, display)]
+    elif inp == "history":
         print('historical!')
         if len(history) == 0:
             print('no history yet')
         else:
             counter = 1
             for i in history:
-                print('roll #', counter, 'was', i[0], 'and the result was', i[1])
+                print('roll #', counter, ' was ', i[0], ' and the result was ', i[1], sep='')
                 counter = counter + 1
-    elif inp == 'close':
-        print('closing time!')
+    elif inp == "close":
+        print("closing time!")
         break
     else:
-        print('invalid command.')
+        print("Invalid Command. Please enter roll, history, or close.")
